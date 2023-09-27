8be914bd8648bb211e1de5d7a13ecc9df0b16270
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-09-27 19:53:52 -0400

    added WASD movement functionality

--- client.js
+++ client.js
@@ -1,24 +1,29 @@
 const net = require("net");
-
+// establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "165.227.47.243", // IP address here,
-    port: 50541 // PORT number here,
+    host: "165.227.47.243",
+    port: 50541
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
   conn.on("data", (data) => {
-    console.log(data)
-  })
+    console.log(data);
+  });
 
   conn.on("connect", () => {
-    console.log("Successfully connected to snek game")
-  })
+    console.log("Successfully connected to snek game");
+  });
+
+  conn.on("connect", () => {
+    conn.write("Name: SNK");
+  });
 
   return conn;
 };
+
 
 module.exports = {
   connect
--- input.js
+++ input.js
@@ -1,9 +1,25 @@
+let connection;
+
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === "\u0077") {
+    connection.write("Move: up")
+  }
+  if (key === "\u0061") {
+    connection.write("Move: left")
+  }
+  if (key === "\u0073") {
+    connection.write("Move: down")
+  }
+  if (key === "\u0064") {
+    connection.write("Move: right")
+  }
 };
-const setupInput = function () {
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
